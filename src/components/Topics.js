4e53d4958edc86d43187e723797a21abdd34b882
Paolo Corti <paolocorti87@gmail.com>
2019-02-15 17:41:28 +0100

    add autocomplete compoentn in topic section

--- src/components/Topics.js
+++ src/components/Topics.js
@@ -3,9 +3,10 @@ import sanityClient from '../lib/sanity';
 import { scaleLinear } from 'd3-scale';
 import { extent } from 'd3-array';
 import { AppContext } from '../appContext';
+import Autocomplete from 'react-autocomplete';
 
 const query = `*[_type=="topic"]{
-  name,
+  _id, name,
   "relatedProjects": count(*[_type=='project' && references(^._id)])
 }`;
 
@@ -15,6 +16,7 @@ const wordScale = scaleLinear()
 
 const Topics = ({ type }) => {
   const [topics, setTopics] = useState([]);
+  const [value, setValue] = useState('');
   const context = useContext(AppContext);
 
   useEffect(() => {
@@ -38,9 +40,42 @@ const Topics = ({ type }) => {
     setTopics(res);
   }
 
+  const matchStateToTerm = (elem, value) => {
+    if (value.length > 0) {
+      return elem.name.toLowerCase().indexOf(value.toLowerCase()) !== -1;
+    }
+  };
+
   return (
     <div className='w-100 h-100 d-flex flex-column'>
-      <div className='w-100 d-flex p-3' />
+      <div className='w-100 p-3'>
+        <Autocomplete
+          getItemValue={item => item.name}
+          items={topics}
+          inputProps={{ className: 'states-autocomplete' }}
+          wrapperStyle={{
+            position: 'relative'
+          }}
+          menuStyle={{
+            backgroundColor: 'white'
+          }}
+          renderItem={(item, isHighlighted) => (
+            <div
+              key={item._id}
+              style={{ background: isHighlighted ? 'lightgray' : 'white' }}
+            >
+              {item.name}
+            </div>
+          )}
+          value={value}
+          shouldItemRender={matchStateToTerm}
+          onChange={(event, value) => setValue(value)}
+          onSelect={val => {
+            context.setSelectedTopic(val);
+            setValue('');
+          }}
+        />
+      </div>
       <div className='w-100 h-100 d-flex flex-wrap p-3 align-items-baseline'>
         {topics
           .sort((a, b) => {
