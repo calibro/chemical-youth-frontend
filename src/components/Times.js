be943a1d573976d7e84400a4120153bda2b68f74
Paolo Corti <paolocorti87@gmail.com>
2019-02-20 10:26:57 +0100

    add height scale

--- src/components/Times.js
+++ src/components/Times.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import sanityClient from '../lib/sanity';
+import { scaleLog } from 'd3-scale';
+import { extent } from 'd3-array';
 
 const query = `*[_type == "project"]{
   endDate, startDate
 }`;
+
+const heightScale = scaleLog().range([30, 120]);
 
 function monthDiff(d1, d2) {
   var months;
@@ -32,14 +36,15 @@ const Times = ({ type }) => {
   }, [type]);
 
   function handleStatusChange(res) {
-    console.log(res);
     setTimes(res);
+    const [min, max] = extent(res, d => d.relatedProjects);
+    heightScale.domain([0, max]);
   }
 
   return (
     <div className='w-100 h-100 d-flex flex-column'>
       <div className='w-100 d-flex p-3' />
-      <div className='w-100 h-100 d-flex flex-column'>
+      <div className='w-100'>
         {times
           .map((time, index) => {
             time.months = time.endDate
@@ -50,8 +55,21 @@ const Times = ({ type }) => {
           .sort((a, b) => b.months - a.months)
           .map((time, index) => {
             return (
-              <div className='p-3' key={index}>
-                {<span>{time.months}</span>}
+              <div
+                className='p-3'
+                key={index}
+                style={{
+                  height: `${heightScale(time.months)}px`,
+                  borderTop: '1px solid #b7b7b7'
+                }}
+              >
+                {
+                  <span>{`${
+                    time.months === 'still running'
+                      ? 'still running'
+                      : time.months + ' months'
+                  }`}</span>
+                }
               </div>
             );
           })}
