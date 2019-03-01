05be0e2b7967d491afa82e420bc1a22fa3d66cf1
uf0 <scnepa222@gmail.com>
2019-03-01 18:12:47 +0100

    Updates

---
+++ precache-manifest.3372f0378179c5d146950e5e46f4584b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c99200c199816678717",
+    "url": "/chemical-youth-frontend/static/js/3.5c99200c.chunk.js"
+  },
+  {
+    "revision": "55179d2555cf654670c6",
+    "url": "/chemical-youth-frontend/static/css/main.900a3af3.chunk.css"
+  },
+  {
+    "revision": "183f2b553a79d003d927",
+    "url": "/chemical-youth-frontend/static/js/runtime~main.183f2b55.js"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/chemical-youth-frontend/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "7564bd60be9ffec30f6d",
+    "url": "/chemical-youth-frontend/static/js/2.7564bd60.chunk.js"
+  },
+  {
+    "revision": "55179d2555cf654670c6",
+    "url": "/chemical-youth-frontend/static/js/main.55179d25.chunk.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/chemical-youth-frontend/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/chemical-youth-frontend/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/chemical-youth-frontend/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "7564bd60be9ffec30f6d",
+    "url": "/chemical-youth-frontend/static/css/2.e03aba38.chunk.css"
+  },
+  {
+    "revision": "5a95b95b1c136891e81a7b2f90000acf",
+    "url": "/chemical-youth-frontend/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/chemical-youth-frontend/precache-manifest.3372f0378179c5d146950e5e46f4584b.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/chemical-youth-frontend/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
