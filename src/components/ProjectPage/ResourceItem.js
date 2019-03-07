348d62d3b6eb308fd8fe2ae446794db91ddf9b83
uf0 <scnepa222@gmail.com>
2019-03-07 18:55:08 +0100

    wip on project page

---
+++ src/components/ProjectPage/ResourceItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import BlockContent from "@sanity/block-content-to-react";
+import styles from "./ProjectPage.module.css";
+
+const ResourceItem = ({ resource, category, url, toggleModal }) => {
+  return resource.private ? (
+    <span className={styles["resource-item"]} onClick={toggleModal}>
+      <span className={styles["resource-category"]}>{`[${category}]`}</span>
+      <BlockContent
+        blocks={resource.name}
+        className={styles["resource-container"]}
+        renderContainerOnSingleChild={true}
+      />
+    </span>
+  ) : (
+    <a
+      href={url}
+      download
+      target="_blank"
+      rel="noopener noreferrer"
+      className={styles["resource-item"]}
+    >
+      <span className={styles["resource-category"]}>{`[${category}]`}</span>
+      <BlockContent
+        blocks={resource.name}
+        className={styles["resource-container"]}
+        renderContainerOnSingleChild={true}
+      />
+    </a>
+  );
+};
+
+export default ResourceItem;
