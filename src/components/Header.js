dddc0cc4e7f517d58789ba563075753b3a1bc42b
uf0 <scnepa222@gmail.com>
2019-03-08 16:51:47 +0100

    wip styling header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { AppContext } from '../appContext';
+import React, { useContext, useEffect, useState } from "react";
+import { AppContext } from "../appContext";
 import {
   Collapse,
   Navbar,
@@ -7,16 +7,16 @@ import {
   NavbarBrand,
   Nav,
   NavItem
-} from 'reactstrap';
-import { withRouter } from 'react-router-dom';
-import { withGetScreen } from 'react-getscreen';
+} from "reactstrap";
+import { withRouter } from "react-router-dom";
+import { withGetScreen } from "react-getscreen";
 
 const Header = ({ history, expanded = true, isMobile }) => {
   const context = useContext(AppContext);
   const [isOpen, toggleOpen] = useState(false);
 
   useEffect(() => {
-    const pathname = history.location.pathname.split('/');
+    const pathname = history.location.pathname.split("/");
     context.setSection(pathname[1]);
   }, [history.location.pathname]);
 
@@ -31,108 +31,107 @@ const Header = ({ history, expanded = true, isMobile }) => {
   };
 
   return (
-    <div>
-      <Navbar
-        color='light'
-        light
-        expand='md'
-        className='header'
-        style={{ height: isMobile() ? 'auto' : expanded ? '70px' : '12px' }}
-      >
-        {expanded && (
-          <React.Fragment>
-            <NavbarBrand>
-              <div
-                className={`navbar-brand cursor-pointer`}
-                onClick={() => goToLanding()}
+    <Navbar
+      color="light"
+      light
+      sticky="top"
+      expand="md"
+      className="header"
+      style={{ height: isMobile() ? "auto" : expanded ? "70px" : "12px" }}
+    >
+      {expanded && (
+        <React.Fragment>
+          <NavbarBrand>
+            <div
+              className={`navbar-brand cursor-pointer`}
+              onClick={() => goToLanding()}
+            >
+              <img src="images/logo-white.svg" width={60} />
+            </div>
+          </NavbarBrand>
+          <NavbarToggler onClick={() => toggleOpen(!isOpen)} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="" navbar>
+              <NavItem className="nav-item">
+                <div className={`header-el-not-link`}>View by:</div>
+              </NavItem>
+              <NavItem
+                className="nav-item"
+                onClick={() => changeSection("chemical")}
               >
-                <img src='images/logo-white.svg' width={60} />
-              </div>
-            </NavbarBrand>
-            <NavbarToggler onClick={() => toggleOpen(!isOpen)} />
-            <Collapse isOpen={isOpen} navbar>
-              <Nav className='' navbar>
-                <NavItem className='nav-item'>
-                  <div className={`header-el-not-link`}>View by:</div>
-                </NavItem>
-                <NavItem
-                  className='nav-item'
-                  onClick={() => changeSection('chemical')}
+                <div
+                  className={`header-el ${
+                    context.section === "chemical" ? "underline" : "none"
+                  }`}
                 >
-                  <div
-                    className={`header-el ${
-                      context.section === 'chemical' ? 'underline' : 'none'
-                    }`}
-                  >
-                    CHEMICAL
-                  </div>
-                </NavItem>
-                <NavItem
-                  className='nav-item'
-                  onClick={() => changeSection('topic')}
+                  CHEMICAL
+                </div>
+              </NavItem>
+              <NavItem
+                className="nav-item"
+                onClick={() => changeSection("topic")}
+              >
+                <div
+                  className={`header-el ${
+                    context.section === "topic" ? "underline" : "none"
+                  }`}
                 >
-                  <div
-                    className={`header-el ${
-                      context.section === 'topic' ? 'underline' : 'none'
-                    }`}
-                  >
-                    TOPIC
-                  </div>
-                </NavItem>
-                <NavItem
-                  className='nav-item'
-                  onClick={() => changeSection('location')}
+                  TOPIC
+                </div>
+              </NavItem>
+              <NavItem
+                className="nav-item"
+                onClick={() => changeSection("location")}
+              >
+                <div
+                  className={`header-el ${
+                    context.section === "location" ? "underline" : "none"
+                  }`}
                 >
-                  <div
-                    className={`header-el ${
-                      context.section === 'location' ? 'underline' : 'none'
-                    }`}
-                  >
-                    LOCATION
-                  </div>
-                </NavItem>
-                <NavItem
-                  className='nav-item'
-                  onClick={() => changeSection('researcher')}
+                  LOCATION
+                </div>
+              </NavItem>
+              <NavItem
+                className="nav-item"
+                onClick={() => changeSection("researcher")}
+              >
+                <div
+                  className={`header-el ${
+                    context.section === "researcher" ? "underline" : "none"
+                  }`}
                 >
-                  <div
-                    className={`header-el ${
-                      context.section === 'researcher' ? 'underline' : 'none'
-                    }`}
-                  >
-                    RESEARCHER
-                  </div>
-                </NavItem>
-                <NavItem
-                  className='nav-item'
-                  onClick={() => changeSection('time')}
+                  RESEARCHER
+                </div>
+              </NavItem>
+              <NavItem
+                className="nav-item"
+                onClick={() => changeSection("time")}
+              >
+                <div
+                  className={`header-el ${
+                    context.section === "time" ? "underline" : "none"
+                  }`}
                 >
-                  <div
-                    className={`header-el ${
-                      context.section === 'time' ? 'underline' : 'none'
-                    }`}
-                  >
-                    TIME
-                  </div>
-                </NavItem>
-                <NavItem
-                  className='nav-item'
-                  onClick={() => changeSection('method')}
+                  TIME
+                </div>
+              </NavItem>
+              <NavItem
+                className="nav-item"
+                onClick={() => changeSection("method")}
+              >
+                <div
+                  className={`header-el ${
+                    context.section === "method" ? "underline" : "none"
+                  }`}
                 >
-                  <div
-                    className={`header-el ${
-                      context.section === 'method' ? 'underline' : 'none'
-                    }`}
-                  >
-                    METHOD
-                  </div>
-                </NavItem>
-              </Nav>
-            </Collapse>
-          </React.Fragment>
-        )}
-      </Navbar>
-    </div>
+                  METHOD
+                </div>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </React.Fragment>
+      )}
+    </Navbar>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -4,29 +4,27 @@ import { BrowserRouter, HashRouter, Route, Switch } from "react-router-dom";
 import Landing from "./components/Landing";
 import Home from "./components/Home";
 import ProjectPage from "./components/ProjectPage";
-import "bootstrap/dist/css/bootstrap.min.css";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import * as serviceWorker from "./serviceWorker";
 
 class App extends React.Component {
   render() {
     return (
       <HashRouter>
-        <div className="w-100 h-100">
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <Route
-              exact
-              path={
-                "/:type(chemical|topic|location|researcher|time|method)?/:selection"
-              }
-              component={Home}
-            />
-            <Route exact path={"/projects/:slug"} component={ProjectPage} />
-          </Switch>
-        </div>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route
+            exact
+            path={
+              "/:type(chemical|topic|location|researcher|time|method)?/:selection"
+            }
+            component={Home}
+          />
+          <Route exact path={"/projects/:slug"} component={ProjectPage} />
+        </Switch>
       </HashRouter>
     );
   }
