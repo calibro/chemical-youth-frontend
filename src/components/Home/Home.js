added39dce205816f98467bb855d4ba4a6f61b7d
Paolo Corti <paolocorti87@gmail.com>
2019-03-15 11:54:41 +0100

    update url composition with commas

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,31 +1,31 @@
-import React, { Component } from "react";
-import queryString from "query-string";
-import Header from "../Header";
-import Chemicals from "../Chemicals";
-import Topics from "../Topics";
-import Researchers from "../Researchers";
-import Locations from "../Locations";
-import Methodologies from "../Methodologies";
-import Times from "../Times";
-import Loader from "../Loader";
-import { AppContext } from "../../appContext";
-import { find, findIndex } from "lodash";
-import styles from "./Home.module.css";
+import React, { Component } from 'react';
+import queryString from 'query-string';
+import Header from '../Header';
+import Chemicals from '../Chemicals';
+import Topics from '../Topics';
+import Researchers from '../Researchers';
+import Locations from '../Locations';
+import Methodologies from '../Methodologies';
+import Times from '../Times';
+import Loader from '../Loader';
+import { AppContext } from '../../appContext';
+import { find, findIndex } from 'lodash';
+import styles from './Home.module.css';
 
-const Projects = React.lazy(() => import("../Projects"));
+const Projects = React.lazy(() => import('../Projects'));
 
 class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      section: "chemical",
+      section: 'chemical',
       setSection: this.setSection,
-      selectedChemical: "",
+      selectedChemical: '',
       setSelectedChemical: this.setSelectedChemical,
-      selectedLocation: "",
+      selectedLocation: '',
       setSelectedLocation: this.setSelectedLocation,
-      selectedTopic: "",
+      selectedTopic: '',
       setSelectedTopic: this.setSelectedTopic,
       selected: [],
       setSelected: this.setSelected,
@@ -35,19 +35,19 @@ class Home extends Component {
 
   setSelectedLocation = location => {
     this.setState({
-      selectedLocation: location === this.state.selectedLocation ? "" : location
+      selectedLocation: location === this.state.selectedLocation ? '' : location
     });
   };
 
   setSelectedTopic = topic => {
     this.setState({
-      selectedTopic: topic === this.state.selectedTopic ? "" : topic
+      selectedTopic: topic === this.state.selectedTopic ? '' : topic
     });
   };
 
   setSelectedChemical = chemical => {
     this.setState({
-      selectedChemical: chemical === this.state.selectedChemical ? "" : chemical
+      selectedChemical: chemical === this.state.selectedChemical ? '' : chemical
     });
   };
 
@@ -80,44 +80,42 @@ class Home extends Component {
   };
 
   componentDidMount() {
-    const pathname = this.props.location.pathname.split("/");
+    const pathname = this.props.location.pathname.split('/');
     this.setSection(pathname[1]);
     const parsed = queryString.parse(this.props.location.search);
-    if (parsed.selected && Array.isArray(parsed.selected)) {
-      parsed.selected.forEach(p => {
-        const selected = {
-          type: pathname[1],
-          value: pathname[1] === "time" ? Number(p) : p
-        };
-        this.toggleSelected(selected);
-      });
-    } else if (parsed.selected && !Array.isArray(parsed.selected)) {
-      const selected = {
-        type: pathname[1],
-        value:
-          pathname[1] === "time" ? Number(parsed.selected) : parsed.selected
-      };
-      this.toggleSelected(selected);
+
+    if (parsed.selected) {
+      const parsedArray = parsed.selected.split(',');
+
+      if (parsedArray.length) {
+        parsedArray.forEach(p => {
+          const selected = {
+            type: pathname[1],
+            value: pathname[1] === 'time' ? Number(p) : p
+          };
+          this.toggleSelected(selected);
+        });
+      }
     }
   }
 
   render() {
-    const pathname = this.props.location.pathname.split("/")[1];
+    const pathname = this.props.location.pathname.split('/')[1];
     return (
       <AppContext.Provider value={this.state}>
         <React.Suspense fallback={<Loader />}>
           <Header />
-          <div className={`container-fluid ${styles["split"]}`}>
-            <div className="row h-100">
-              <div className={`col-12 col-md-6 ${styles["col-viz"]}`}>
-                {pathname === "chemical" && <Chemicals />}
-                {pathname === "topic" && <Topics />}
-                {pathname === "location" && <Locations />}
-                {pathname === "researcher" && <Researchers />}
-                {pathname === "time" && <Times />}
-                {pathname === "method" && <Methodologies />}
+          <div className={`container-fluid ${styles['split']}`}>
+            <div className='row h-100'>
+              <div className={`col-12 col-md-6 ${styles['col-viz']}`}>
+                {pathname === 'chemical' && <Chemicals />}
+                {pathname === 'topic' && <Topics />}
+                {pathname === 'location' && <Locations />}
+                {pathname === 'researcher' && <Researchers />}
+                {pathname === 'time' && <Times />}
+                {pathname === 'method' && <Methodologies />}
               </div>
-              <div className={`col-12 col-md-6 ${styles["col-projects"]}`}>
+              <div className={`col-12 col-md-6 ${styles['col-projects']}`}>
                 <Projects />
               </div>
             </div>
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export const parseQueryParams = selection => {
   return selection
     .map((v, i) => {
-      return i === 0 ? `?selected=${v.value}` : `&selected=${v.value}`;
+      return i === 0 ? `?selected=${v.value}` : `,${v.value}`;
     })
     .join('');
 };
