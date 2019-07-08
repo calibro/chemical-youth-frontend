bdec88c72b60c33ab734ae241d722c7eb42f77f6
uf0 <scnepa222@gmail.com>
2019-07-08 17:52:13 +0200

    Updates

---
+++ precache-manifest.f77cc916343183badc876e0a2dabbb9c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "378dae0465513331e860",
+    "url": "/chemical-pathways/static/js/3.378dae04.chunk.js"
+  },
+  {
+    "revision": "eb68f94e2ae597b8ab99",
+    "url": "/chemical-pathways/static/css/main.20bc0615.chunk.css"
+  },
+  {
+    "revision": "13b445c117a0df7bba01",
+    "url": "/chemical-pathways/static/js/runtime~main.13b445c1.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/chemical-pathways/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "cb5aef9b111d1ce597b3",
+    "url": "/chemical-pathways/static/js/2.cb5aef9b.chunk.js"
+  },
+  {
+    "revision": "eb68f94e2ae597b8ab99",
+    "url": "/chemical-pathways/static/js/main.eb68f94e.chunk.js"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/chemical-pathways/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/chemical-pathways/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/chemical-pathways/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "cb5aef9b111d1ce597b3",
+    "url": "/chemical-pathways/static/css/2.a6cdb491.chunk.css"
+  },
+  {
+    "revision": "82ff4b910701a8670e43774174a5dc51",
+    "url": "/chemical-pathways/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/chemical-youth-frontend/precache-manifest.af35455b8cc076cd0db6017c0770d44c.js"
+  "/chemical-pathways/precache-manifest.f77cc916343183badc876e0a2dabbb9c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/chemical-youth-frontend/index.html", {
+workbox.routing.registerNavigationRoute("/chemical-pathways/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
